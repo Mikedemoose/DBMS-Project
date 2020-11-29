fcccaaab13a4eea67776b0cda0339d0d7229170f
Mikedemoose <mikedemoose13@gmail.com>
2020-11-29 13:49:35 +0530

    created about us and contacts php files and linked all pages

---
+++ About.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="styles/About.css" type="text/css">
+	<link rel="stylesheet" href="styles/icofont/icofont.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css">
+
+	<title>About-Us</title>
+</head>
+<body>
+	<div class="navigation">
+		<img src="images/logo.png" name="logo">
+		<a name="about" href="About.php">About-Us </a>
+		<text name="1">|</text>
+		<a name="contact" href="Contacts.php">Contact-Us </a>
+		<text name="2">|</text>
+		<a name="login" href="Login.php">Log-In</a>
+		<a name="signup" href="register.php" name="signup">Sign-Up </a>
+		<text name="3">|</text>
+	</div>
+		<div class="post">
+
+			<p name="heading">About-Us</p>
+
+			<div class="sub">
+				<text>Blah Blah....</text>
+			</div>
+
+		</div>
+
+		</body>
+		</html>
